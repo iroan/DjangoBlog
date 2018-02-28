a930b54b5d8852407f93391488f4baa4755bc4e5
wangkaixuan <2654189525@qq.com>
2018-02-27 23:54:21 -0800

    完善了分类和标签待

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     url(r'^$',views.index, name='index'),
-    url(r'^post/(?P<pk>[0-9]+)/$',views.detail,name='detail')
+    url(r'^post/(?P<pk>[0-9]+)/$',views.detail,name='detail'),
+    # [0-9]{4}:0到9的数字出现4次
+    # [0-9]{1,2}:0到9的数字出现1,2次
+    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$',views.archives,name='archives'),
+    url(r'^category/(?P<pk>[0-9]+)/$',views.category,name='category')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from markdown import markdown
 from django.shortcuts import render,get_object_or_404
-from .models import Post
+from .models import Post,Category
 # Create your views here.
 
 def index(request):
@@ -14,3 +14,15 @@ def detail(request,pk):
                                                  'markdown.extensions.toc',
                                                  ])
     return render(request,'blog/detail.html',context={'post':post})
+
+def archives(request,year,month):
+    # created_time__month是作为参数的写法,如果是类实例中调用属性方法则用created_time.month
+    post_list = Post.objects.filter(created_time__year = year,
+                                    created_time__month=month).order_by('-created_time')
+    return render(request,'blog/index.html',context={'post_list':post_list})
+
+def category(request,pk):
+    # 参数是一个模型类
+    cate = get_object_or_404(Category,pk=pk)
+    post_list = Post.objects.filter(category = cate).order_by('-created_time')
+    return render(request,'blog/index.html',context={'post_list':post_list})
